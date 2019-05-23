ca652ffae5273f9364a45fb3dac7e3a3b92372f5
luojinrong <840841939@qq.com>
2019-05-23 19:34:21 +0800

    ac r560-D

--- codeforces/round560/D.cpp
+++ codeforces/round560/D.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 const int maxn(305);
-int t,n,d[maxn],ans;
+ll t,n,d[maxn],ans;
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
@@ -9,7 +10,7 @@ int main(){
 	cin>>t;
 	while(t--){
 		cin>>n;
-		for(int i=0;i<n;i++){
+		for(ll i=0;i<n;i++){
 			cin>>d[i];
 		}
 		sort(d,d+n);
@@ -19,11 +20,27 @@ int main(){
 		else{
 			ans=d[n/2]*d[n/2-1];
 		}
-		for(int i=0;i<n/2;i++){
+		for(ll i=0;i<n/2;i++){
 			if(d[i]*d[n-1-i]!=ans){
 				ans=-1;
 				break;
 			}
 		}
+		if(ans!=-1){
+			ll tmp=ll(sqrt(ans)),num=0;
+			if(tmp*tmp==ans){
+				num=1;
+				tmp--;
+			}
+			for(ll i=2;i<=tmp;i++){
+				if(ans%i==0){
+					num+=2;
+				}
+			}
+			if(num!=n){
+				ans=-1;
+			}
+		}
+		cout<<ans<<endl;
 	}
 }
